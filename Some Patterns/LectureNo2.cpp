8af1ce34d3949e0a789343fc0501153c072de79e
Souradeep Ghosh <116977697+unicornjohn@users.noreply.github.com>
2023-04-09 23:52:01 +0530

    Add files via upload

---
+++ Some Patterns/LectureNo2.cpp
@@ -0,0 +1,117 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+    
+//     /*
+//     int a =123;
+
+//     cout << a << endl;
+
+//     char b ='v';
+
+//     cout << b << endl;
+
+    
+//     // Here true means 1
+//     // false means 0
+//     bool bl = true;
+//     cout << bl << endl;
+
+//     bool bn = false;
+//     cout << bn << endl;
+
+//     float f = 1.2;
+//     cout << f <<endl;
+
+//     double g = 1.7829;
+//     cout << g << endl;
+
+//     int size = sizeof(g) ;
+//     cout << "The size of f is : "<< size << endl;
+
+
+// int a = 'a';
+// cout << a << endl;
+
+// char ch = 98;
+// cout << ch << endl;
+
+
+// int a = 5;
+// cout << a << endl;
+
+// // print only +ve and -ve number both
+
+// int b = -6;
+// cout << b << endl;
+
+// //print -ve number 
+
+// unsigned int c = 5;
+// cout << c << endl;
+
+// bool g = false;
+// cout  << g << endl;
+
+// double s = 2.0/5;
+// cout << s << endl;
+ 
+// */
+// /*
+// if 0 ------>  False
+// if 1 ------>  True
+
+// */
+
+// // int a = 2;
+// // int b = 3;
+
+// // bool first = (a==b);
+// // cout << first << endl;
+
+// // bool second = (a<b);
+// // cout << second << endl;
+
+// // bool third = (a>b);
+// // cout << third << endl;
+
+// // bool fourth = (a<=b);
+// // cout << fourth << endl;
+
+// // bool fifth = (a>=b);
+// // cout << fifth << endl;
+
+// // bool sixth = (a!=b);
+// // cout << sixth << endl;
+
+
+
+
+// int a = 'b';
+// cout << a << endl;
+
+// char ch =  102;
+// cout << ch << endl;
+
+// // this is call type casting
+
+
+
+
+
+
+
+
+// }
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    double a = 10;
+    int size = sizeof(a);
+    cout << "Size of a is " << size << endl;
+}
+
