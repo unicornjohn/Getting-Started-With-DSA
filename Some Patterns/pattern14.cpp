71d2a4e178c3457e6697a85c5caa5d72e8196c16
Souradeep Ghosh <116977697+unicornjohn@users.noreply.github.com>
2023-04-20 04:01:12 +0530

    Add files via upload

---
+++ Some Patterns/pattern14.cpp
@@ -0,0 +1,34 @@
+/*
+A
+ABA
+ABCBA
+ABCDCBA
+ABCDEDCBA
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for(int row=0;row<n;row++)
+    {
+        int col;
+        for(int col=0;col<row+1;col++)
+        {
+            int ans=col+1;
+            char ch = ans+'A'-1;
+            cout <<ch;
+        }
+        // Reverse counting
+        for(int col=row;col>=1;col--)
+        {
+            int ans =col;
+            char ch = ans+'A' -1;
+            cout << ch;
+        }
+        cout << endl;
+    }
+
+}
