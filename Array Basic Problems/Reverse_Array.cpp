d367bd7201dfa6e173846f6fde4ad549c6356a28
Souradeep Ghosh <116977697+unicornjohn@users.noreply.github.com>
2023-05-12 12:02:08 +0530

    Add files via upload

--- Array Basic Problems/Reverse_Array.cpp
+++ Array Basic Problems/Reverse_Array.cpp
@@ -4,21 +4,19 @@
 using namespace std;
 int main()
 {
-    int arr[]={9,2,3,4,5,6,7};
-    int size = 7;
+    int arr[]={20,30,45,67,12,43,67,21};
+    int size = 8;
 
     int start = 0;
     int end = size - 1;
 
     while(start<=end){
-        //step :1
         swap(arr[start],arr[end]);
-        
         start++;
         end --;
     }
-    for(int i = 0;i<size ;i++)
+    for(int i=0;i<size;i++)
     {
-        cout << arr[i] << " ";
+        cout<<arr[i]<<" ";
     }
 }
